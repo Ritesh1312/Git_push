542d039f56c8ea55b7c9ffed271e46ef08f63692
Ritesh <riteshrk1312@gmail.com>
2023-09-15 06:45:58 +0200

    python code added

---
+++ team7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Welcome to Chatbot of Group 7")
+print("(^_^)")
+name = input("Enter your name buddy (^_^) ")
+age = int(input("Enter your age "))
+if age>= 18:
+    print(name+ " can enter the pub")
+else:
+    print("Sorry, You can't enter here")
+
+
+# In[ ]:
+
+
+
+
